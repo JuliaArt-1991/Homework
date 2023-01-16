4b272701a468b85c0ffab155af957746dd206454
Julia <artamon-julia@ya.ru>
2023-01-16 14:58:41 +0700

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
