a05a9754ca0839b4c72ba7e5315adeffbe1f4b35
Julia <artamon-julia@ya.ru>
2023-01-16 15:39:38 +0700

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n / n;
+while (m <= n)
+{
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m} ");
+        m = m + 1;
+    }
+    else
+    {
+        m = m + 1;
+    }
+}
